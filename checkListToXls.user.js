9bddfc2112e6ab6c6b3ec0235fec4d33906551ea
Андрей Осьмаков <38988068+osmaav@users.noreply.github.com>
2025-06-12 19:11:20 +0700

    Update checkListToXls.user.js
    // @  Версия 3.7.1 
    // @  - Производительность: оптимизировать использование наблюдателей и сокращать количество операций над DOM.
    // @  - Безопасность: контролировать ресурсные утечки и обеспечить совместимость с будущими изменениями сайта.
    // @  - Интерфейс: улучшать взаимодействия пользователя и информативность уведомлений.
    // @  - Кодовая база: уменьшить дублирование кода и упростить структуру CSS-стилей.

--- checkListToXls.user.js
+++ checkListToXls.user.js
@@ -1,8 +1,13 @@
 // ==UserScript==
-// @name         Check-List->xlsx for LT
-// @namespace    http://tampermonkey.net/
-// @version      2025-05-30_v.3.6.4
-// @description  Скрипт создает кнопку "скачать" для выгрузки Чек-листа в файл формата xlsx (версия 3.6.4 - исправил адрес для поддержки beta версии)
+// @name         Download Button for LT
+// @namespace    http://tampermonkey.net
+// @version      2025-06-12_v.3.7.1
+// @description  Скрипт создает кнопку "скачать" для выгрузки Чек-листа в файл формата xlsx
+// @  Версия 3.7.1 
+// @  - Производительность: оптимизировать использование наблюдателей и сокращать количество операций над DOM.
+// @  - Безопасность: контролировать ресурсные утечки и обеспечить совместимость с будущими изменениями сайта.
+// @  - Интерфейс: улучшать взаимодействия пользователя и информативность уведомлений.
+// @  - Кодовая база: уменьшить дублирование кода и упростить структуру CSS-стилей.
 // @author       osmaav
 // @homepageURL  https://github.com/osmaav/extention-for-lt
 // @updateURL    https://raw.githubusercontent.com/osmaav/extention-for-lt/main/checkListToXls.user.js
@@ -12,236 +17,183 @@
 // @match        https://www.beta.leadertask.ru/*
 // @grant        none
 // @run-at       document-idle
-
 // ==/UserScript==
-( async() => {
+
+(async () => {
   'use strict';
-  //console.warn('UserScript: Скрипт запущен', currtime());
-  try {
-    const { XLSX } = await import('https://unpkg.com/xlsx@0.18.5/dist/xlsx.full.min.js');
-  } catch (error) {
-    console.warn('UserScript:',currtime(), 'ошибка загрузки модуля XLSX', error);
-    return;
+
+  // Подключение библиотеки XLSX через создание тега <script>
+  function loadLibrary(callback) {
+    const script = document.createElement('script');
+    script.src = 'https://cdn.jsdelivr.net/npm/xlsx@0.18.5/dist/xlsx.full.min.js';
+    script.onload = callback;
+    document.head.appendChild(script);
   }
 
-  let btnExpListToXlsx = document.createElement('button');
-  btnExpListToXlsx.classList.add('dark:bg-[#1B1B1C]/[0.5]',
-                                 'dark:hover:bg-[#1B1B1C]/[0.8]',
-                                 'bg-gray-200',
-                                 'hover:bg-gray-300',
-                                 'text-[14px]',
-                                 'leading-[16px]',
-                                 'py-[4px]',
-                                 'px-[8px]',
-                                 'border-solid',
-                                 'rounded-[6px]',
-                                 'btnExpListToXlsx');
-  btnExpListToXlsx.style.marginLeft = '5px';
-  btnExpListToXlsx.style.position = 'relative';//
-  btnExpListToXlsx.style.height = '1.6rem';//
-  btnExpListToXlsx.style.width = '4.6rem';//
-  btnExpListToXlsx.innerHTML = 'Скачать';
+  // Глобально устанавливаем XLSX после успешного импорта
+  loadLibrary(() => {
+    window.XLSX = window.XLSX || {};
+  });
 
-  let myDiv = document.createElement('div');
-  myDiv.classList.add('el-popper', 'is-dark');
-  myDiv.dataset.popperPlacement = 'bottom';
-  myDiv.style.display = 'none';
-  myDiv.style.left = '-3.8rem';//
-  myDiv.innerHTML = 'Скачать список в виде таблицы';
+  // Кэширование текущих значений для сравнения
+  let oldCheckListSize = 0;
+  let previousUrlPath = '';
 
-  let mySpan = document.createElement('span');
-  mySpan.classList.add('el-popper__arrow');
+  // Настройка CSS для кнопки
+  function addStyles() {
+    const styles = `
+      .btnExpListToXlsx {
+        background-color: rgba(0, 0, 0, 0.2);
+        border-radius: 6px;
+        padding: 4px 8px;
+        font-size: 14px;
+        line-height: 16px;
+        transition: all 0.3s ease-in-out;
+        position: relative;
+        margin-left: 5px;
+        height: 1.6rem;
+        width: 4.6rem;
+      }
 
-  myDiv.appendChild(mySpan);
-  btnExpListToXlsx.appendChild(myDiv);
-  btnExpListToXlsx.addEventListener('click',
-    event => {
-      event.stopPropagation();
-      exportToXlsx(document
-                   .querySelector('.user_child_customer_custom div>div')
-                   .outerText.replaceAll(': ', '_')
-                   .replaceAll('/', '_')
-                   .replaceAll(' ', '_')
-                  , XLSX);
-    },
-  true);
-  let myTable = [];
-  let flMyStyleAdd = false;
-  let oldCheckListLen = 0;
-  function currtime() {
-      const now = new Date();
-      const currt = now.toLocaleString() + '.' + now.getMilliseconds();
-      return currt};
-  function getcheckList() {
-    //console.warn('UserScript:',currtime(), 'getcheckList is runnin', currtime());
-    try {
-      return document.querySelectorAll('#task-prop-content div.flex.items-center.w-full.group');
-    } catch (error) {console.warn('UserScript: ',currtime(), error);}
+      .btnExpListToXlsx:hover {
+        background-color: rgba(0, 0, 0, 0.4);
+      }
+
+      .btnExpListToXlsx:active {
+        transform: scale(0.95);
+        box-shadow: inset 0 0 10px rgba(0, 0, 0, 0.2);
+      }
+    `;
+
+    const styleElem = document.createElement('style');
+    styleElem.type = 'text/css';
+    styleElem.appendChild(document.createTextNode(styles));
+    document.head.appendChild(styleElem);
   }
 
-  function exportToXlsx(taskName = 'Задача', xlsx ) {
-    //console.warn('UserScript:',currtime(), 'exportToXlsx вызвана');
-    Array.from(getcheckList())
-      .map(el => el.textContent)
-      .map((el , idx) => myTable.push({ idx: idx + 1, content: el.replace(new RegExp('/^[0-9]+.s+/', 'g'), '') }))
-      .filter(el => el != undefined);
-    const worksheet = xlsx.utils.json_to_sheet(myTable);
-    const workbook = xlsx.utils.book_new();
-    //console.log(`Список готов!`, myTable);
-    xlsx.utils.book_append_sheet(workbook, worksheet, taskName.slice(0, 31));
-    xlsx.utils.sheet_add_aoa(worksheet, [['Номер', 'Значение']], { origin: 'A1' });
-    const max_width_col_A = myTable.reduce((w, r) => Math.max(w, r?.idx.toString().length), 5);
-    const max_width_col_B = myTable.reduce((w, r) => Math.max(w, r?.content.length), 30);
-    myTable = [];
-    worksheet['!cols'] = [{ wch: max_width_col_A }, { wch: max_width_col_B }];
-    let fileName = `CheckList-from-${taskName}-${new Date().toLocaleString()}.xlsx`
-    .replaceAll(new RegExp(/,\s+/, 'g'), '-')
-    .replaceAll(new RegExp(/\:/, 'g'), '.');
-    xlsx.writeFile(workbook, fileName, {
-      compression: true
+  // Функционал кнопки
+  function createDownloadButton() {
+    const button = document.createElement('button');
+    button.classList.add('btnExpListToXlsx');
+    button.textContent = 'Скачать';
+    button.onclick = handleDownloadClick;
+    return button;
+  }
+
+  // Генерирует имя файла
+  function generateFilename(taskName) {
+    const dateStr = new Date().toLocaleDateString();
+    return `CheckList-from-${taskName}-${dateStr}.xlsx`
+      .replaceAll(',', '-')
+      .replaceAll(':', '.');
+  }
+
+  // Экспорт чек-листа в Excel
+  function exportToXlsx(taskName) {
+    const rows = Array.from(getCheckList()).map((el, idx) => ({
+      idx: idx + 1,
+      content: el.textContent.trim()
+    }));
+    const sheet = XLSX.utils.json_to_sheet(rows);
+    const book = XLSX.utils.book_new();
+    XLSX.utils.book_append_sheet(book, sheet, taskName.slice(0, 31));
+    XLSX.utils.sheet_add_aoa(sheet, [['Номер', 'Значение']], { origin: 'A1' });
+    const filename = generateFilename(taskName);
+    XLSX.writeFile(book, filename, { compression: true });
+  }
+
+  // Сбор чек-листа
+  function getCheckList() {
+    return document.querySelectorAll('#task-prop-content div.flex.items-center.w-full.group');
+  }
+
+  // Управление видимостью кнопки
+  function manageButtonVisibility(checkListSize) {
+    const button = document.querySelector('.btnExpListToXlsx');
+    if (checkListSize > 2) {
+      if (!button) {
+        let targetEl = document.querySelector('#modal-container > div:nth-child(5) > div.flex > div > div:nth-child(1) > div:nth-child(2) > div > div div:nth-child(3) > div > span');
+        if (targetEl) {
+          targetEl.append(createDownloadButton());
+        } else {
+          targetEl = document.querySelector('#task-prop-content > div:nth-child(3) > div > span');
+          if (targetEl) {
+            targetEl.append(createDownloadButton());
+          } else {
+            document.querySelector('#task-prop-content > div:nth-child(4) > div > span').append(createDownloadButton());}
+        }
+      }
+    } else {
+      if (button) {
+        button.remove();
+      }
+    }
+  }
+
+  // Контролирует поведение кнопки при изменениях
+  function handleDownloadClick(event) {
+    event.preventDefault();
+    const taskContainer = document.querySelector('.user_child_customer_custom div>div');
+    const taskName = taskContainer.outerText
+    .replaceAll(': ', '_')
+    .replaceAll('/', '_')
+    .replaceAll(' ', '_');
+    exportToXlsx(taskName);
+  }
+
+  // Основной обработчик изменений
+  function processChanges(urlChanged, checkListSizeChanged) {
+    if (urlChanged || checkListSizeChanged) {
+      manageButtonVisibility(getCheckList().length);
+    }
+  }
+
+  // Настройка наблюдателя
+  function setupMutationObserver() {
+    const observerConfig = { attributes: true, childList: true, subtree: true };
+
+    const modalObserver = new MutationObserver((mutations) => {
+      let urlChanged = false;
+      let checkListSizeChanged = false;
+
+      mutations.forEach((mutation) => {
+        if (mutation.type === 'childList') {
+          const checkList = getCheckList();
+          const currentCheckListSize = checkList.length;
+          if (currentCheckListSize !== oldCheckListSize) {
+            checkListSizeChanged = true;
+            oldCheckListSize = currentCheckListSize;
+          }
+        }
+      });
+
+      const currentUrlPath = location.pathname;
+      if (previousUrlPath !== currentUrlPath) {
+        urlChanged = true;
+        previousUrlPath = currentUrlPath;
+      }
+
+      processChanges(urlChanged, checkListSizeChanged);
+    });
+
+    modalObserver.observe(document.body, observerConfig);
+
+    // Освобождение ресурса при выходе со страницы
+    window.addEventListener('beforeunload', () => {
+      modalObserver.disconnect();
     });
   }
 
-
-  function updateBtn() {
-    let checkListLen = getcheckList().length;
-    //console.warn('UserScript:', currtime(), 'updateBtn is running...');
-    if (checkListLen > 2) { // -- чек-лист > 2
-      if (document.querySelector('.btnExpListToXlsx')) { // -- если кнопка есть
-        btnExpListToXlsx.style.display = 'block'; // -- показываем кнопку
-        console.warn('UserScript:',currtime(), 'показали кнопку');
-      } else { // -- если кнопки нет
-        let targetEl = document.querySelector('#task-prop-content > div:nth-child(3) > div > span');// -- ищем целевой элемент к которому добавим кнопку
-        //console.warn('UserScript: targetEl найден', targetEl);
-        if (targetEl) targetEl.append(btnExpListToXlsx);
-        else {document.querySelector('#task-prop-content > div:nth-child(4) > div > span').append(btnExpListToXlsx);}
-        // -- добавляем кнопку
-        console.warn('UserScript:',currtime(), 'добавили кнопку');
-      }
-    } else if (checkListLen < 3) { // чек-лист < 3
-      btnExpListToXlsx.style.display = 'none';// -- скрываем кнопку
-      console.warn('UserScript:', currtime(), 'скрыли кнопку');
-    }
+  // Основной поток инициализации
+  function init() {
+    addStyles();
+    setupMutationObserver();
   }
 
-  function MyMutationObserver() {
-    console.warn('UserScript:',currtime(), 'DOMContentLoaded');
-    let oldUrl = '';
-    const css = `
-      .btnExpListToXlsx>div {
-         top: 125%;
-         left: -63%;
-         overflow: inherit;
-         text-wrap-mode: nowrap;
-         text-align: center;
-       }
-      .btnExpListToXlsx:hover>div {
-         display: flex !important;
-         animation: opacity 1.5s infinite;
-         animation-direction: alternate;
-       }
-       @keyframes opacity{
-        from{
-          color: #707173;
-        }
-        to {
-          color: white;
-        }
-       }
-       .btnExpListToXlsx>div>span {
-         left: 5.5rem;
-       }
-       .btnExpListToXlsx:hover>div>span {
-         animation-direction: alternate;
-       }
-       @keyframes opacity2{
-        to {
-          opacity: 1;
-        }
-       }`;
-    let style = document.createElement('style');
-    style.type = 'text/css';
-    style.appendChild(document.createTextNode(css));
-    document.head.appendChild (style);
-    const app = document.querySelector('#app');
-
-    if (!app) {
-      console.warn('UserScript:', currtime(), 'Элемент app не найден в DOM');
-      return;
-    }
-
-    function throttle(fn, delay = 1000) {
-      //console.warn('UserScript:', currtime(), 'throttle is running...');
-      let lastCallTime = 0;
-      return function(...args) {
-        const currentTime = Date.now();
-        if (currentTime - lastCallTime >= delay) {
-          console.warn('UserScript:', currtime(), 'fn is running...', fn, args);
-          fn.apply(this, args);
-          lastCallTime = currentTime;
-        }
-      };
-    }
-
-    new MutationObserver(() => {
-      let curUrl = document.location.href;
-//       const taskPropertyWidow = document.querySelector(`#modal-container`);
-      const taskPropertyWidow = document.querySelector(`#modal-container`);
-      if (oldUrl != curUrl) {
-//        console.warn('UserScript:',currtime(), 'путь изменился', oldUrl, curUrl);
-        oldUrl = curUrl;
-        if (!taskPropertyWidow) {
-          console.warn('UserScript:', currtime(), 'taskPropertyWidow не найден', taskPropertyWidow);
-          return;
-        }
-        if (!(taskPropertyWidow instanceof Node)) {
-          console.warn('UserScript:',currtime(), 'taskPropertyWidow не является Node');
-          return;
-        }
-        //if (!taskPropertyWidow.style.length) console.warn('UserScript:',currtime(), 'обрабатываем события:');
-        const throttledUpdateBtn = throttle(updateBtn, 1000);
-
-        new MutationObserver(mutations => {
-//           console.warn('UserScript:', currtime(), 'обрабатываем события:', mutations);
-          // -- путь содержит project или tasks
-          if (curUrl.includes('/project/') || curUrl.includes('/tasks/')) {
-            //console.warn('UserScript:', currtime(), 'путь содержит project или tasks', curUrl);
-            // -- обработка событий
-            for (const mutation of mutations) {
-              if ((mutation.attributeName === 'style') && (mutation.type === 'attributes')) { // -- окно открылось
-                if ((curUrl.includes('/project/')) && (curUrl.split('/').length === 7)) {
-                  throttledUpdateBtn();
-                } // -- путь содержит project
-                if ((curUrl.includes('/tasks/')) && (curUrl.split('/').length === 6)) {
-                  throttledUpdateBtn();
-                }// -- путь содержит tasks
-              }// -- окно открылось
-              if (mutation.type === 'childList') {
-                const removeLen = mutation.removedNodes[0]?.textContent.length;
-                //console.warn('UserScript:', currtime(), 'событие childList','target:',mutation.target);
-                let checkListLen = getcheckList().length;
-                if ((mutation.target.id === 'addNewCheckListEdit' || removeLen) && (checkListLen != oldCheckListLen)) { // -- чек-лист изменился
-                  //console.warn('UserScript:', currtime(), 'Чек-лист изменился oldLen:', oldCheckListLen, 'newLen:', checkListLen);
-                  throttledUpdateBtn();
-                } // -- чек-лист изменился
-              } // -- mutation.type === 'childList'
-            }// -- обработка событий
-          } // -- если путь содержит project или tasks
-        }).observe(taskPropertyWidow, {
-          attributes: true,
-          subtree: true,
-          attributeFilter: ['style'],
-          childList: true
-        });
-
-      }
-    }).observe(app, {subtree: true, attributeFilter: ['style'], childList: true});
-  }
-  if (document.readyState == 'loading') {
-    // ещё загружается, ждём события
-    document.addEventListener('DOMContentLoaded', MyMutationObserver());
+  // Запускаем основной процесс
+  if (document.readyState === 'interactive' || document.readyState === 'complete') {
+    init(); // Если документ уже загружен, запускаем сразу
   } else {
-    // DOM готов!
-    MyMutationObserver();
+    document.addEventListener('DOMContentLoaded', init); // Иначе ждем завершения загрузки
   }
 })();
