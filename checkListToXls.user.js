49f8216bf053b746c1d4f106296a544fa46ed7db
Андрей Осьмаков <38988068+osmaav@users.noreply.github.com>
2025-06-24 15:34:16 +0700

    Update checkListToXls.user.js
    // @  Версия 3.7.4
    // @  - исправления: скорректировал адрес // @match https://beta.leadertask.ru/*

--- checkListToXls.user.js
+++ checkListToXls.user.js
@@ -1,17 +1,17 @@
 // ==UserScript==
 // @name         Download Button for LT
 // @namespace    http://tampermonkey.net
-// @version      2025-06-13_v.3.7.3
+// @version      2025-06-13_v.3.7.4
 // @description  Скрипт создает кнопку "скачать" для выгрузки Чек-листа в файл формата xlsx
-// @  Версия 3.7.3
-// @  - Вычисления: скорректировал получение списка.
+// @  Версия 3.7.4
+// @  - исправления: скорректировал адрес // @match https://beta.leadertask.ru/*
 // @author       osmaav
 // @homepageURL  https://github.com/osmaav/extention-for-lt
 // @updateURL    https://raw.githubusercontent.com/osmaav/extention-for-lt/main/checkListToXls.user.js
 // @downloadURL  https://raw.githubusercontent.com/osmaav/extention-for-lt/main/checkListToXls.user.js
 // @supportURL   https://github.com/osmaav/extention-for-lt/issues
 // @match        https://www.leadertask.ru/web/*
-// @match        https://www.beta.leadertask.ru/*
+// @match        https://beta.leadertask.ru/*
 // @grant        none
 // @run-at       document-idle
 // ==/UserScript==
