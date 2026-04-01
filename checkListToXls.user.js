5460a6dc9307aff19986a65f380fc9f48b3cb5fc
Андрей Осьмаков <38988068+osmaav@users.noreply.github.com>
2026-04-01 13:41:33 +0700

    Update checkListToXls.user.js
    изменил иконку и цвета

--- checkListToXls.user.js
+++ checkListToXls.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
-// @name         Download Button for LT 4.5.2
-// @version      2025-12-13_v.4.5.2
+// @name         Download Button for LT 4.5.3
+// @version      2026-04-01_v.4.5.3
 // @description  Скрипт создает кнопку для скачивания Чек-листа в файл формата xlsx
 // @author       osmaav
 // @updateURL    https://raw.githubusercontent.com/osmaav/extention-for-lt/main/checkListToXls.user.js
@@ -36,7 +36,6 @@
   function addStyles() {
     const styles = `
       .btnExpListToXlsx {
-        --border-width: 1px;
         user-select: none;
         -moz-user-select: none;
         -khtml-user-select: none;
@@ -45,10 +44,9 @@
         font-size: 0.9em;
         position: relative;
         z-index: 0;
-        padding: 0.1em 0.5em;
+        padding: 0.5em 0.5em;
         left: 0.5em;
-        top: -3px;
-        border: var(--border-width, 1px) solid transparent;
+        top: 0px;
         border-radius: 6px;
         transition: all 0.3s ease-in-out;
       }
@@ -70,8 +68,13 @@
         inset: -1px;
       }
 
-      html.dark .btnExpListToXlsx::before{
-          background: #111;
+      .svgColor{
+         color: #0F0;
+      }
+        
+      .btnExpListToXlsx:hover{
+        .svgColor{
+         color: #FF4C00;
         }
       }
     `;
@@ -99,15 +102,19 @@
         'bg-[#EEEEF1]',
         'dark:bg-[#0A0A0C]',
         'opacity-50',
-        'hover:opacity-100'
+        'hover:opacity-100',
     );
 
     // Отображаем изображение в качестве иконки загрузки
-    const iconImg = `
-        <img src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABgAAAAYCAYAAADgdz34AAAACXBIWXMAAAsTAAALEwEAmpwYAAABY0lEQVR4nN2Uv0oDQRDG09gpgmCjYmMj+hRaKb6AjQ9gaWNsBEEQn8PChxAxePd9k6ibGYWAtahlQloLiWy8YC45ktuYWDjwVfPnt7Mzu4XCvzPSdkB9o1hrsLQBsYNgAKivw4vbj2gnYR0kiXnjKNaC6PlEAQyBjApgG5JjJrkB1LgPQK2PDRCUl381bajGs5oSCOg4RapbED31isu63UlI7311l3SrpBWDARC7r1Selrwo9tALAPWzVKpNU+yd1ONggFdU0bU4tvXuBKZX8QJwKxC7zPg+osEdUBHHbtkLYuwHaIPUI4q9iNTmkpwPiB0C1YXM4ikAbIO0s289bva2nMxgz3eJsu4nf1GxMMx+s0VITt7umBpnAvzGjAbRKPiBTuQl/ykAOa4KYrc+VsQWQWv2+an1KNL5kecBsWsf65ybAvUuAwDvC72FTHPOzVKs3AUn8DwzluJpiN6QdtVb/At3PVyfwezqAwAAAABJRU5ErkJggg==" alt="xls-export">
+    const iconSVG = `
+        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" class="svgColor">
+  <path d="M21 15V19C21 19.5304 20.7893 20.0391 20.4142 20.4142C20.0391 20.7893 19.5304 21 19 21H5C4.46957 21 3.96086 20.7893 3.58579 20.4142C3.21071 20.0391 3 19.5304 3 19V15" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path>
+  <path d="M7 10L12 15L17 10" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path>
+  <path d="M12 15V3" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path>
+</svg>
     `;
 
-    button.innerHTML = iconImg;
+    button.innerHTML = iconSVG;
     button.title = 'Скачать чек-лист'; // Альтернативный текст для доступности
 
     // Присваиваем обработчик нажатия кнопки
